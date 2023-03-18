d7495f8924e392a3c073bf4b38a30536b1109ac3
RhettBooher13 <rhettbooher@gmail.com>
2023-03-18 13:09:05 -0500

    Attempt to make collisions work.

--- Lab 5/Assets/Scripts/FirstPersonCamera.cs
+++ Lab 5/Assets/Scripts/FirstPersonCamera.cs
@@ -13,22 +13,9 @@ public class FirstPersonCamera : MonoBehaviour
 
     void Update()
     {
-        MouseAiming();
         KeyboardMovement();
     }
 
-    void MouseAiming()
-    {
-        // get the mouse inputs
-        float y = Input.GetAxis("Mouse X") * turnSpeed;
-        rotX += Input.GetAxis("Mouse Y") * turnSpeed;
-
-        // clamp the vertical rotation
-        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
-
-        // rotate the camera
-        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
-    }
 
     void KeyboardMovement()
     {
